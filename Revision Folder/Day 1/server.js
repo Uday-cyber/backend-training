5702b5bed4293c7cbe71cb39b324d77d9e1df2fe
Your Name <you@example.com>
2025-08-27 16:49:40 +0530

    In Day 1

--- Revision Folder/Day 1/server.js
+++ Revision Folder/Day 1/server.js
@@ -1,2 +1,7 @@
-const name = process.argv[2];
-console.log(`Hello, ${name}! Welcome to Node.js `);
+// const name = process.argv[2];
+// console.log(`Hello, ${name}! Welcome to Node.js `);
+
+const num1 = Number(process.argv[2]);
+const num2 = Number(process.argv[3]);
+
+console.log(`Addition of ${num1} and ${num2} is: `, num1 + num2);
