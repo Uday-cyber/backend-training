1067dd775e90ee18dc85f1418b97980e77800777
Your Name <you@example.com>
2025-11-19 16:31:34 +0530

    Day 14 - Refresh token & Cloud File Storage Feature Added

---
+++ Day 14/backend/routes/userRoute.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { getUsers, loginUser, refreshToken, registerUser } from '../controllers/userController.js';
+
+const userRoute = express.Router();
+
+userRoute.post('/register', registerUser);
+
+userRoute.get('/', getUsers);
+
+userRoute.post('/login', loginUser);
+
+userRoute.post('/refresh', refreshToken);
+
+
+export default userRoute;
