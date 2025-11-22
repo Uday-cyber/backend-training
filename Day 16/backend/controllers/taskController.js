ead3ff2844c22c6f0cfe6bab6e9a7dc179c60475
Your Name <you@example.com>
2025-11-22 18:39:41 +0530

    Day 16 - MongoDB Populate and Aggregate Feature Added

--- Day 16/backend/controllers/taskController.js
+++ Day 16/backend/controllers/taskController.js
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 
 import Task from "../models/Tasks.js";
-import { error } from 'console';
 
 export const createTask = async (req, res) => {
     try{
